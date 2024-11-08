e411832a1fed8c1b9eb8669d3ffe6e40b916c9af
eigen-spaced <saneyar.k@gmail.com>
2024-11-08 14:14:07 +1100

    Add alternating turn logic

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -12,21 +12,19 @@
                            "O" "X" nil
                            nil nil nil])
 
-(def starting-turn (atom "X"))
+(def player-starts-as-x (atom true))
 
 (defn format-board [board]
   (->> board
        (map #(if (= % nil) " " %))
        (partition 3)
        (map #(str/join " | " %))
-       (str/join "\n") ;; Format each row as a string
-       (str "\n")))
+       (str/join "\n"))) ;; Format each row as a string
 
 (defn update-board [board pos turn]
   (assoc board pos turn))
 
 (defn get-user-input []
-  (println "Player turn")
   (print "Enter a position (0-8):")
   (flush)
   (let [input (try (Integer/parseInt (read-line))
@@ -47,7 +45,6 @@
       (update-board board position turn))))
 
 (defn ai-turn [board turn]
-  (println "AI turn")
   (let [position (choose-move board turn)]
     (update-board board position turn)))
 
@@ -57,12 +54,16 @@
     (is-draw? board) "Match is a draw"
     :else nil))
 
-(defn game-loop [board starting-mark]
-  (let [player-mark starting-mark
-        ai-mark (if (= "X" player-mark) "O" "X")]
+(defn game-loop [board player-is-x]
+  (let [player-mark (if player-is-x "X" "O")
+        ai-mark (if player-is-x "O" "X")]
     (loop [current-board board
-           current-mark player-mark]
+           ;; X goes first
+           current-mark "X"]
       (println (format-board current-board))
+      (println) ;; Print a newline after the board
+      (println (if (= current-mark player-mark) "Player turn" "AI turn"))
+      ;; determine if the player or AI goes first
       (let [updated-board (if (= current-mark player-mark)
                             (player-turn current-board current-mark)
                             (ai-turn current-board ai-mark))
@@ -74,14 +75,10 @@
 (defn -main
   [& args]
   (println "Xs and Os, built with Clojure!")
-  (loop
-    ;; Ensure "X" always starts each game
-   (when (= @starting-turn "O")
-      ;; Toggle to "X" if "O" would start
-     (swap! starting-turn toggle-turn))
+  (loop []
     (println "Starting new game")
     (let [new-board (create-new-board)]
-      (game-loop new-board @starting-turn))
+      (game-loop new-board @player-starts-as-x))
     ;; Toggle starting turn for the next game
-    (swap! starting-turn toggle-turn)
+    (swap! player-starts-as-x not)
     (recur)))
--- src/tic_tac_toe/helpers.clj
+++ src/tic_tac_toe/helpers.clj
@@ -6,14 +6,9 @@
   (if (= current-turn "X") "O" "X"))
 
 (def win-patterns
-  [[0 1 2]  ;; rows
-   [3 4 5]
-   [6 7 8]
-   [0 3 6]  ;; columns
-   [1 4 7]
-   [2 5 8]
-   [0 4 8]  ;; diagonal
-   [2 4 6]])
+  [[0 1 2] [3 4 5] [6 7 8] ;; rows
+   [0 3 6] [1 4 7] [2 5 8] ;; columns
+   [0 4 8] [2 4 6]])       ;; diagonal 
 
 (def test-board-x-wins [nil "X" "O"
                         "O" "X" nil
